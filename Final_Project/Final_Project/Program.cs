4cbfbdbac588aec9c32d7e7e6ff33b094b221ce2
NirMekin <nir.mekin@gmsil.com>
2016-09-20 16:09:07 +0300

    Hello World Test

---
+++ Final_Project/Final_Project/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (;;)
+            {
+                Console.WriteLine("Please enter your name");
+                string temp = Console.ReadLine();
+                Console.WriteLine("Hi, " + temp);
+                if (temp.Equals("Hello World"))
+                {
+                    Console.WriteLine("Bye World");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You are not the king so... try again");
+                }
+            }
+            Console.WriteLine("Bye Bye")
+            Console.ReadLine();
+        }
+    }
+}
