138d2adb95ed2c83fcf375b10877d63310a1ae39
Yogev911 <Yogev911@gmail.com>
2016-10-02 09:46:30 +0300

    Update the manu

--- Final_Project/Final_Project/Program.cs
+++ Final_Project/Final_Project/Program.cs
@@ -21,19 +21,21 @@ namespace ConsoleApplication1
                 if (service.GetWD() == null) continue;
                 Console.Clear();
                 Console.WriteLine("Hey Sir, You requested to see the weather in {0} in country {1}", service.GetWD().cityName, service.GetWD().country);
-                Console.WriteLine("Or to be more specific it's in coord {0} , {0} ", service.GetWD().coordLat, service.GetWD().coordLon);
+                Console.WriteLine("Or to be more specific it's in coord {0} , {1} ", service.GetWD().coordLat, service.GetWD().coordLon);
                 Console.WriteLine("The last update I have is from {0}", service.GetWD().lastupdate);
-                Console.WriteLine("the weather looks {0} ", service.GetWD().weather);
-                Console.WriteLine("The sun will rise in {0} and set at {1}", service.GetWD().sunRise, service.GetWD().sunSet);
+                Console.WriteLine("It seems to be {0} today ", service.GetWD().weather);
+                Console.WriteLine("The sun will rise at {0} and set at {1}", service.GetWD().sunRise, service.GetWD().sunSet);
                 Console.WriteLine("the tempature now is {0} and it should be between {1} - {2}", service.GetWD().tempature, service.GetWD().tempatureMin, service.GetWD().tempatureMax);
                 Console.WriteLine("the wind speed is {0} MPH", service.GetWD().windSpeed);
-                Console.WriteLine("the pressure (hmm wtf is this..) is {0} phd?!", service.GetWD().pressure);
+                Console.WriteLine("the pressure is {0} hPa", service.GetWD().pressure);
                 Console.WriteLine("cloud status is {0} and that's great", service.GetWD().clouds);
-                Console.WriteLine("the humidity is {0}% ", service.GetWD().humidity);
+                Console.WriteLine("the humidity is {0}% \n\n\n\n", service.GetWD().humidity);
                 service.ClearWeatherData();
                 
             }
-            Console.WriteLine("bye bye and thank you for using Nir and Yogev code");
+            Console.WriteLine("Copyrights Shenkar - Software Engineering");
+            Console.WriteLine("Course Programming Languages Oct 2016 - By Mr. Life Michael ");
+            Console.WriteLine("Written by Yogev Heskia and Nir Mekin ");
         }
     }
 }
